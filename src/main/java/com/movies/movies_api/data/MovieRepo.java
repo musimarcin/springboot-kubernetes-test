726d7592963ee70835d86a68e4516dc62ddfd06e
musimarcin <marcinm994@gmail.com>
2024-09-21 15:56:32 +0200

    working test containers (fixed issue - too few resources for virtual machine)

--- src/main/java/com/movies/movies_api/data/MovieRepo.java
+++ src/main/java/com/movies/movies_api/data/MovieRepo.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface MovieRepo extends JpaRepository<Movie, Long> {
 
+    //for getting only specified columns
     @Query("select new com.movies.movies_api.data.MovieDTO(m.id, m.title, m.releaseYear, m.addedWhen) from Movie m")
     Page<MovieDTO> findMovies(Pageable pageable);
 
--- src/test/java/com/movies/movies_api/api/MovieManagerTest.java
+++ src/test/java/com/movies/movies_api/api/MovieManagerTest.java
@@ -3,19 +3,13 @@ package com.movies.movies_api.api;
 import com.movies.movies_api.data.MovieRepo;
 import com.movies.movies_api.data.entity.Movie;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.Instant;
 import java.util.ArrayList;
@@ -27,42 +21,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//mock mvc for testing without server as if handling with real http
 @AutoConfigureMockMvc
-@Testcontainers
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:tc:postgresql:14-alpine:///testdb"
+})
 class MovieManagerTest {
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     private MockMvc mvc;
 
     @Autowired
     MovieRepo movieRepo;
 
+    private List<Movie> movieList;
     @BeforeEach
-    void setUp() {
+    void addTestData() {
         movieRepo.deleteAllInBatch();
-        List<Movie> movieList = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            movieList.add(new Movie(null, "Movie " + i + 1, 1988 + i, Instant.now()));
-        }
+        movieList = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++)
+            movieList.add(new Movie(null, "Movie " + i, 1988 + i, Instant.now()));
+
         movieRepo.saveAll(movieList);
     }
-
-    @Container
-    private static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:9.6.8")
-            .withDatabaseName("databasename");
-
-    @DynamicPropertySource
-    static void configureDataSource(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
-    }
-
     //parameterized test with given inputs
     @ParameterizedTest
     @CsvSource({
-            "1,10,0,true",
-            "2,10,1,false"
+            "1,10,3,0,true",
+            "3,10,3,2,false"
     })
-    void shouldGetMovies(int pageNo, int totalElements, int totalPages, int currentPage, boolean hasNext) throws Exception {
+    void testGetMovies(int pageNo, int totalElements, int totalPages, int currentPage, boolean hasNext) throws Exception {
         mvc.perform(get("/api/movies?page="+pageNo))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.totalElements", equalTo(totalElements)))
